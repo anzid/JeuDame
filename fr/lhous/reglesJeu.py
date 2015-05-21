a5653e5a0591750a05e46be81d14bff77a58ed55
LRACHDI <rachdi.lhoussaine@gmail.com>
2015-05-21 22:03:44 +0200

    First commit

---
+++ fr/lhous/reglesJeu.py
@@ -0,0 +1,17 @@
+'''
+Created on 21 mai 2015
+
+@author: rachdi
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
